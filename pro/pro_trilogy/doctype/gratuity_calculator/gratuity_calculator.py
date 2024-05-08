7d2b9f618fb999edf2ba0b5f7294cf8d870c22de
alaalsalam <alaalsalam101@gmail.com>
2024-05-08 09:42:24 +0200

    add doctype

---
+++ pro/pro_trilogy/doctype/gratuity_calculator/gratuity_calculator.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, NSFSS.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class GratuityCalculator(Document):
+	pass
